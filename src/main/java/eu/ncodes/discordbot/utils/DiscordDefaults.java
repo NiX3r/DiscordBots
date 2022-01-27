7f18434f2e9d184149e4d436ccad567d9c08f058
Daniel Iliev <47360939+nCodesDotEU@users.noreply.github.com>
2022-01-27 20:49:02 +0100

    Fix building project

--- src/main/java/eu/ncodes/discordbot/utils/DiscordDefaults.java
+++ src/main/java/eu/ncodes/discordbot/utils/DiscordDefaults.java
@@ -1,7 +1,5 @@
 package eu.ncodes.discordbot.utils;
 
-import com.vdurmont.emoji.EmojiParser;
-
 public class DiscordDefaults {
 
     public static String roleAtMember = "662003511801675786";
@@ -9,6 +7,5 @@ public class DiscordDefaults {
     public static String roleSupport = "921420870449242162";
     public static String rolenCodesAI = "920454719695093760";
     public static String categorySupport = "852878611378995230";
-    public static String channelWelcome = "611985124057284621";
 
 }
