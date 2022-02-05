c77b3c7875a5fd1d836aee873b4191c52e1ce18e
Daniel Iliev <47360939+nCodesDotEU@users.noreply.github.com>
2022-02-05 19:48:54 +0100

    Do Challenger progress

--- src/main/java/eu/ncodes/discordbot/bots/challenger/Challenger.java
+++ src/main/java/eu/ncodes/discordbot/bots/challenger/Challenger.java
@@ -2,6 +2,9 @@ package eu.ncodes.discordbot.bots.challenger;
 
 import eu.ncodes.discordbot.nextends.BotExtend;
 import eu.ncodes.discordbot.utils.LogSystem;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.activity.ActivityType;
+import org.javacord.api.entity.permission.Permissions;
 
 public class Challenger extends BotExtend {
 
@@ -9,6 +12,17 @@ public class Challenger extends BotExtend {
         setToken(token);
         setPrefix("nChallenger");
         LogSystem.log(getPrefix(), "instance created", new Throwable().getStackTrace()[0].getLineNumber(), new Throwable().getStackTrace()[0].getFileName(), new Throwable().getStackTrace()[0].getMethodName());
+    }
+
+    @Override
+    public void initializeBot(){
+        setBot(new DiscordApiBuilder().setToken(getToken()).setAllIntents().login().join());
+        LogSystem.log(getPrefix(), "bot is ready on : " + getBot().createBotInvite(Permissions.fromBitmask(8)), new Throwable().getStackTrace()[0].getLineNumber(), new Throwable().getStackTrace()[0].getFileName(), new Throwable().getStackTrace()[0].getMethodName());
+        initializeLogListeners();
+        //getBot().addMessageCreateListener(new nMessageCreateListener());
+        getBot().updateActivity(ActivityType.LISTENING, " ncodes bots");
+        LogSystem.log(getPrefix(), "bot initialized and turned on", new Throwable().getStackTrace()[0].getLineNumber(), new Throwable().getStackTrace()[0].getFileName(), new Throwable().getStackTrace()[0].getMethodName());
+
     }
 
 }
