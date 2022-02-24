79d6ae898ec9533cb9f4bbb2d295a9ce48d135e1
Daniel Iliev <47360939+nCodesDotEU@users.noreply.github.com>
2022-02-24 11:35:44 +0100

    Do progress on voice timer

---
+++ src/main/java/eu/ncodes/discordbot/bots/giveawayer/timers/VoiceTimer.java
@@ -0,0 +1,16 @@
+package eu.ncodes.discordbot.bots.giveawayer.timers;
+
+import eu.ncodes.discordbot.bots.giveawayer.Giveawayer;
+import eu.ncodes.discordbot.utils.DiscordUtils;
+
+import java.util.TimerTask;
+
+public class VoiceTimer extends TimerTask {
+
+    @Override
+    public void run() {
+        Giveawayer giveawayer = ((Giveawayer) DiscordUtils.bots.get("giveawayer"));
+        for ()
+    }
+
+}
